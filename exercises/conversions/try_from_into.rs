7c6fc65773eabd3b4b2c06fbffa5747fe2993af8
Tom Wallis <thomaswallis0@gmail.com>
2020-06-08 08:57:28 -0400

    Day 9

--- exercises/conversions/try_from_into.rs
+++ exercises/conversions/try_from_into.rs
@@ -10,7 +10,6 @@ struct Person {
     age: usize,
 }
 
-// I AM NOT DONE
 // Your task is to complete this implementation
 // in order for the line `let p = Person::try_from("Mark,20")` to compile
 // and return an Ok result of inner type Person.
@@ -28,6 +27,15 @@ struct Person {
 impl TryFrom<&str> for Person {
     type Error = String;
     fn try_from(s: &str) -> Result<Self, Self::Error> {
+        if s.len() == 0 {
+            Err(String::from("0 len"))
+        } else {
+            let tokens: Vec<&str> = s.split(",").collect();
+            match tokens[1].parse::<usize>() {
+                Ok(age) => Ok(Person{name: String::from(tokens[0]), age: age}),
+                Err(_) => Err(String::from("bad age"))
+            }
+        }
     }
 }
 
